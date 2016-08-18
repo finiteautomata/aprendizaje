416358a89b6dbb3fed9b9a533de2746690052be7
Juan Manuel Pérez <jmperez.85@gmail.com>
2016-08-18 18:04:49 -0300

    Agrego tests.

---
+++ 01_taller_arboles/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+from utils import entropy
+
+class EntropyTest(unittest.TestCase):
+    def test_for_zero_entropy(self):
+        self.assertAlmostEqual(entropy([0, 0, 0]), 0.0)
+
+    def test_for_max_entropy(self):
+        # We need two bits to represent this
+        self.assertAlmostEqual(entropy([0, 1, 2, 3]), 2.0)
+
+    def test_for_just_one_element(self):
+        self.assertAlmostEqual(entropy([1]), 0.0)
+
+if __name__ == '__main__':
+    unittest.main()
